d7be265d4b2223c1f4cbe4c0499c969e7b3dd8d0
michaeloverman <overman.michael@gmail.com>
2016-10-03 14:41:05 -0400

    this is missing shit

--- src/Parser.java
+++ src/Parser.java
@@ -12,8 +12,14 @@ public class Parser {
 //    private static String codes = "data/shortCoW.txt";  // used for testing
     private static String mida = "data/MIDA_4.01.csv";
     private static String midb = "data/MIDB_4.01.csv";
-    private static String testCode = "data/small_test_graph";
+    private static String testCode = "data/small_test_graph"; // used for testing
 
+    /**
+     * Method to parse file of disputes.
+     * Use AFTER parsing file of Country Codes, and States have been created.
+     * @param ag Takes an AlliesGraph containing States, with no edges. Uses
+     *           disputes from file to create the edges.
+     */
     public static void parseDisputes(AlliesGraph ag) {
         HashMap<Integer, Dispute> map = new HashMap<>();
 
@@ -24,7 +30,7 @@ public class Parser {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String header = scanner.nextLine();
+        String header = scanner.nextLine(); // capture header line, dispose of as junk
         while (scanner.hasNext()) {
             String[] data = scanner.nextLine().split(",");
             int id = Integer.parseInt(data[0]);
@@ -34,10 +40,12 @@ public class Parser {
             Dispute d = ag.getDispute(id);
             d.addState(data[2], Integer.parseInt(data[10]));
         }
-
-
     }
 
+    /**
+     * Method to parse data file of Country Codes.
+     * @param ag Takes an empty AlliesGraph, and fills the nodes with State objects.
+     */
     public static void parseStateCodes(AlliesGraph ag) {
         Path filePath = Paths.get(codes);
         Scanner scanner = null;
@@ -46,31 +54,29 @@ public class Parser {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String s = scanner.nextLine();
+        String s = scanner.nextLine(); // capture header line as junk to dispose of
         while (scanner.hasNext()) {
             s = scanner.nextLine();
             ag.addState(parseCodeLine(s));
-
         }
-
-
     }
+
+    /**
+     * Helper method to parse each line of the Country Codes data
+     * @param data one line of data, as a String
+     * @return Returns State, created from data in the line.
+     */
     private static State parseCodeLine(String data) {
         String[] pieces = data.split("[,]| ", 3);
-//        System.out.println(pieces.length);
-//        for (String p : pieces) {
-//            System.out.println(p);
-//        }
-//        System.out.println("");
-//        if (pieces.length > 3) {
-//            String s = "";
-//            for (int i = 2; i < pieces.length; i++) {
-//                s += pieces[i];
-//            }
-//            pieces[2] = s;
-//        }
+
         return new State(pieces[0], Integer.parseInt(pieces[1]), pieces[2]);
     }
+
+    /**
+     * Utility for parsing the simple sample graph, for testing of Graph class methods. Useless now
+     * except for potential future work.
+     * @param g Takes an empty Graph object, and populates nodes and edges from data file
+     */
     public static void parseTestCode(Graph g) {
         Path filePath = Paths.get(testCode);
         Scanner scanner = null;
@@ -86,8 +92,6 @@ public class Parser {
             g.addVertex(a);
             g.addVertex(b);
             g.addEdge(a, b);
-
         }
-
     }
 }
